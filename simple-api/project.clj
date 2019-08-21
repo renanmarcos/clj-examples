2c72c3eed771a7b1fe7660ccb5654e4afa860216
Renan Marcos Ferreira <renan.ferreira18@fatec.sp.gov.br>
2019-08-21 17:15:36 -0300

    Some adjusts

--- simple-api/project.clj
+++ simple-api/project.clj
@@ -8,7 +8,7 @@
                  [ring/ring-core "1.7.1"]
                  [ring/ring-json "0.5.0"]]
   :plugins [[lein-ring "0.12.5"]]
-  :ring {:handler simple-api.handler/app}
+  :ring {:handler simple-api.handler/app :port 2782 :open-browser? false}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring/ring-mock "0.3.2"]]}})
--- simple-api/src/simple_api/employers.clj
+++ simple-api/src/simple_api/employers.clj
@@ -1,18 +1,32 @@
 (ns simple_api.employers)
 
+;; Our atom collection
 (def employers-collection (atom []))
 
-(defn get-employers []
-    (str @employers-collection))
+;; Generate a new id
+(defn new-id [] 
+    (if (= (count @employers-collection) 0) 1
+            (count @employers-collection)))
 
-(defn get-employer [id]
-    (str @employers-collection))
+;; GET /employers
+(defn get-employers [] @employers-collection)
 
+;; GET /employers/:id
+(defn get-employer [id] 
+    (first 
+        (filter #(= (:id %) id) @employers-collection)))
+
+;; POST /employers
 (defn add-employer [name job]
-    (str @employers-collection))
+    (let [response {:name name :job job}]
+        (swap! employers-collection conj {:id (new-id) :name name :job job})
+        response))
 
+;; PUT /employers/:id - TODO
 (defn update-employer [id name job]
-    (str @employers-collection))
+    (let [response {:id id :name name :job job}]
+        response))
 
+;; DELETE /employers/:id - TODO
 (defn delete-employer [id]
-    (str @employers-collection))
+    (swap! employers-collection #(remove (fn [x] (= (:id x) id)) @employers-collection)))
